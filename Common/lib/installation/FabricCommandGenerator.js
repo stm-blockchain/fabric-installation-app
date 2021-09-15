9180a48dc2274093de0fdb64ab5e645b2ddd89e6
anilhelvaci <anilhelvaci96@gmail.com>
2021-09-15 18:25:02 +0300

    #57 A we service implemented returning the value of the sequence number and the version of a chaincode deployed on a channel.

--- Common/lib/installation/FabricCommandGenerator.js
+++ Common/lib/installation/FabricCommandGenerator.js
@@ -19,6 +19,7 @@ const Commands = {
         INSTALL: "peer lifecycle chaincode install",
         QUERY_INSTALLED: "peer lifecycle chaincode queryinstalled -O json",
         APPROVE: "peer lifecycle chaincode approveformyorg",
+        QUERY_APPROVED: "peer lifecycle chaincode queryapproved --output json",
         CHECK_COMMIT_READINESS: "peer lifecycle chaincode checkcommitreadiness",
         COMMIT: "peer lifecycle chaincode commit"
     }
@@ -118,6 +119,19 @@ module.exports = {
             return command.join(" ");
         } catch (e) {
             throw new Errors.CommandGenerationError(`ERROR GENERATING APPROVE CMD`, e);
+        }
+    },
+    generateQueryApprovedCommand(channelName, ccName) {
+        try {
+            const command = [Commands.PEER.QUERY_APPROVED,
+                `-C ${channelName}`,
+                `-n ${ccName}`,
+                Commands.OS.TO_STDOUT
+            ];
+
+            return command.join(" ");
+        } catch (e) {
+            throw new Errors.CommandGenerationError(`ERROR GENERATING QUERYAPPROVED CMD`, e);
         }
     },
     generateCommitReadinessCommand(chaincodeConfig) {
